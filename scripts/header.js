12812c810ad58b189049b0fa1b3d5439df5fa1f7
CJ <chostett@pm.me>
2023-06-12 20:36:21 +0900

    Split JavaScript into header.js and main.js.

---
+++ scripts/header.js
@@ -0,0 +1,23 @@
+// Navigation HTML. Can be changed and will reflect on all pages.
+let headerHTML = `<div class='container'>
+      <h1><a href='index.html'>illuminesce.</a></h1>
+      <ul>
+        <li><a href='blog/index.html'>blog</a></li>
+        <li><a href='work.html'>work</a></li>
+        <li><a href='talks.html'>talks</a></li>
+        <li><a href='contact.html'>contact</a>&nbsp;|&nbsp;</li>
+        <li><a href='index_jp.html'>日本語</a></li>
+      </ul>
+    </div>`;
+
+let header = document.getElementById("header");
+
+// Checks to see if #header is present.
+if (header) {
+  // Inject navigation links into header.
+  header.innerHTML = headerHTML;
+  // Takes our pathname, gets the last part of the pathname (url) and applies .selected class
+  let url = window.location.pathname.split('/');
+  let selectedLink = header.querySelector(`a[href$="${url[url.length - 1]}"]`);
+  selectedLink.classList.add('selected');
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
-// Navigation HTML. Can be changed and will reflect on all pages.
-let headerHTML = `<div class='container'>
-      <h1><a href='index.html'>illuminesce.</a></h1>
-      <ul>
-        <li><a href='blog/index.html'>blog</a></li>
-        <li><a href='work.html'>work</a></li>
-        <li><a href='talks.html'>talks</a></li>
-        <li><a href='contact.html'>contact</a>&nbsp;|&nbsp;</li>
-        <li><a href='index_jp.html'>日本語</a></li>
-      </ul>
-    </div>`;
-
 // Tag filtering function. Takes a selected tag and shows all blog posts with that tag (done as div classes)
 window.onload = () => {
   const tags = document.querySelectorAll('#tags > button');
@@ -46,17 +34,5 @@ window.onload = () => {
       }
 
     })
-  }
-
-  let header = document.getElementById("header");
-
-  // Checks to see if #header is present.
-  if (header) {
-    // Inject navigation links into header.
-    header.innerHTML = headerHTML;
-    // Takes our pathname, gets the last part of the pathname (url) and applies .selected class
-    let url = window.location.pathname.split('/');
-    let selectedLink = header.querySelector(`a[href$="${url[url.length - 1]}"]`);
-    selectedLink.classList.add('selected');
   }
 }
