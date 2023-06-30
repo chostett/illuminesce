8aeeef72bc6d286ad81a2d89372559bee4c59513
CJ <chostett@pm.me>
2023-06-30 16:47:58 +0900

    Added RGST Japanese version.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,8 @@
 // Navigation HTML. Can be changed and will reflect on all pages.
 let footerHTML = `<div class='col'>
       <h3>socials.</h3>
-      <p><a href="https://www.twitter.com/chostett/" target="_blank">twitter</a></p>
+      <p><a href="https://www.twitter.com/chostett/" target="_blank">design twitter</a></p>
+      <p><a href="https://www.twitter.com/its_illuminesce/" target="_blank">games twitter</a></p>
       <p><a href="https://instagram.com/illuminesce" target="_blank">instagram</a></p>
       <p><a href="https://illuminesce.cohost.org/" target="_blank">cohost</a></p>
       <p><a href="https://www.tumblr.com/illuminesce" target="_blank">tumblr</a></p>
