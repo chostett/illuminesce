4506df9a233d31ea1adcdd8acc2b856b92de97b2
CJ <chostett@pm.me>
2023-10-07 18:44:52 +0800

    More mobile formatting.

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 // Navigation HTML. Can be changed and will reflect on all pages.
 let headerHTML = `<div class='container'>
-      <h1 class="title"><a href='index.html'>illuminesce.</a></h1>
-      <ul class="header-links">
+      <h1><a href='index.html'>illuminesce.</a></h1>
+      <ul>
         <li><a href='blog/index.html'>blog</a></li>
         <li><a href='games.html'>games</a></li>
         <li><a href='talks.html'>talks</a></li>
