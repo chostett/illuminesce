648f03fd1832729036e066bbc4f2c74f8fc14e59
CJ <chostett@pm.me>
2024-08-27 15:13:22 +0900

    Added a draft blog post on Design Matters.

--- talks/header.js
+++ talks/header.js
@@ -3,13 +3,11 @@ let headerHTML = `<div class='container'>
       <h1><a href='../index.html'>illuminesce.</a></h1>
       <ul>
         <li><a href='../blog/index.html'>blog</a></li>
-        <li><a href='https://illuminesce.itch.io'>games</a></li>
-        <li><a href='https://glassart.neocities.org'>glass art</a></li>
-        <li class="selected"><a href='../talks.html'>talks</a></li>
         <li><a href='../resources.html'>resources</a></li>
+        <li class="selected"><a href='../talks.html'>talks</a></li>
+        <li><a href='https://illuminesce.itch.io'>games</a></li>
+        <li><a href='https://glassart.neocities.org'>glass</a></li>
         <li><a href='../contact.html'>contact</a></li>
-        <li><a href='../rss.xml'>rss</a>&nbsp;|&nbsp;</li>
-        <li><a href='../index_jp.html'>日本語</a></li>
       </ul>
     </div>`;
 
