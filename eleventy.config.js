20ff250691e7923aa4cf8e8f58a817de0cbfb797
CJ <chostett@pm.me>
2025-01-19 14:45:25 +0900

    Updated font name.

--- eleventy.config.js
+++ eleventy.config.js
@@ -100,7 +100,7 @@ export default async function(eleventyConfig) {
     satoriOptions: {
       fonts: [
         {
-          name: 'Zen Maru Gothic Japanese',
+          name: 'Zen Maru Gothic',
           data: fs.readFileSync('./_includes/font-file/zen-maru/zen-maru-gothic-japanese-300-normal.woff'),
           weight: 300,
           style: 'normal',
