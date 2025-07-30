b8a9d73dc0e6211cd5a76a43f3c3e6d58275e58e
CJ <chostett@pm.me>
2025-07-30 16:24:15 +0900

    added dinosaur, removed item from drafts

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,11 +3,7 @@ export default {
 	url: "https://illuminesce.net/",
 	language: "en",
 	description: `
-I'm a creative, inquisitive person who likes talking with people.
-
-That means you.
-
-I live in Tokyo and co-run Studio Terranova, a co-operative video game and software consulting company. If you're in Tokyo, let me know. I love a good coffee.`,
+I'm a creative, inquisitive person who likes talking with people. That means you.`,
 	author: {
 		name: "CJ",
 		email: "cj@studioterranova.net",
