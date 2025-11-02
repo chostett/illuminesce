feee818189d28615969b4d17eced1626f81c1c48
CJ <chostett@pm.me>
2025-11-02 19:57:57 +0900

    Fixed the links in archives.

--- public/talks/header.js
+++ public/talks/header.js
@@ -5,8 +5,8 @@ let headerHTML = `<div class='container'>
         <li><a href='../blog/index.html'>blog</a></li>
         <li><a href='../resources.html'>resources</a></li>
         <li class="selected"><a href='../talks.html'>talks</a></li>
-        <li><a href='https://illuminesce.itch.io'>games</a></li>
-        <li><a href='https://glassart.neocities.org'>glass</a></li>
+        <li><a href='https://illuminesce.net/links'>links</a></li>
+        <li><a href='https://illuminesce.net/rss.xml'>rss</a></li>
         <li><a href='../contact.html'>contact</a></li>
       </ul>
     </div>`;
